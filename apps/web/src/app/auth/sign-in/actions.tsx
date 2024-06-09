8959527d11a0d23e1e38a8455217ebe376efd603
Nathan <nathanferreira00@hotmail.com>
2024-06-09 18:29:16 -0300

    feat(web): login with email and password

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,15 +1,55 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+
 import { signInWithEmailAndPassword } from '@/http/sign-in-with-email-and-password'
 
-export async function signInWithEmailAndPasswordAction(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address' }),
+  password: z.string().min(1, { message: 'Please, provide your password' }),
+})
 
-  const { accessToken } = await signInWithEmailAndPassword({
-    email: String(email),
-    password: String(password),
-  })
+export async function signInWithEmailAndPasswordAction(formData: FormData) {
+  const formDataParseResult = signInSchema.safeParse(
+    Object.fromEntries(formData),
+  )
 
-  console.log(accessToken)
-  return accessToken
+  if (!formDataParseResult.success) {
+    const validationErrors = formDataParseResult.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors: validationErrors }
+  }
+
+  const { email, password } = formDataParseResult.data
+
+  try {
+    const { accessToken } = await signInWithEmailAndPassword({
+      email,
+      password,
+    })
+
+    cookies().set('token', accessToken, {
+      path: '/',
+      maxAge: 60 * 60 * 60 * 24 * 7, // 7 days
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
