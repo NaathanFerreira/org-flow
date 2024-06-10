9c73d2c28a546f562d28cbff9bf352fbb06d043b
Nathan <nathanferreira00@hotmail.com>
2024-06-10 13:00:25 -0300

    feat(web): app header with authenticated user and orgs select

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,24 @@
+import { Slash } from 'lucide-react'
+import Image from 'next/image'
+
+import orgIcon from '@/assets/org-icon.svg'
+
+import { OrganizationSwitcher } from './organization-switcher'
+import { ProfileButton } from './profile-button'
+
+export function Header() {
+  return (
+    <header className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image src={orgIcon} className="size-6 dark:invert" alt="Org" />
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </header>
+  )
+}
