53be2ceba1a52214b9d50c6817c8b3d481b9163e
Nathan <nathanferreira00@hotmail.com>
2024-06-11 11:22:35 -0300

    feat(web): create organization page

---
+++ apps/web/src/http/create-organization.ts
@@ -0,0 +1,24 @@
+import { api } from './api-client'
+
+interface createOrganizationRequest {
+  name: string
+  domain: string | null
+  shouldAttachUsersByDomain: boolean
+}
+
+type createOrganizationResponse = void
+
+export async function createOrganization({
+  name,
+  domain,
+  shouldAttachUsersByDomain,
+}: createOrganizationRequest): Promise<createOrganizationResponse> {
+  console.log(domain)
+  await api.post('organizations', {
+    json: {
+      name,
+      domain,
+      shouldAttachUsersByDomain,
+    },
+  })
+}
